60b763e420ec2a9a9bdf74c6bca150cf9e91a7f6
Julius Biskopstoe <jbiskopstoe@gmail.com>
2019-04-21 22:10:03 +0100

    initial commit - simple node app

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["@typescript-eslint", "jest"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    indent: "off",
+    "@typescript-eslint/indent": ["error", 2, { SwitchCase: 1 }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off"
+  }
+};
